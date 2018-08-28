1a60bd6d842c4924b346a6bcf8f425d6b4fc7417
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-08-28 05:45:10 +0000

    task2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''
+A Python script that, using this REST API,
+for a given employee ID, exports data in the json format.
+'''
+import json
+import requests
+from sys import argv
+
+
+def output_json(employee_id):
+    '''
+    The function to retrieve employee information.
+    '''
+    emp_id = argv[1]
+    emp = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(emp_id)).json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                        .format(emp_id)).json()
+    jsonfile = emp_id + '.json'
+    todo_list = []
+    for todo in todos:
+        task = {}
+        task['task'] = todo.get('title')
+        task['completed'] = todo.get('completed')
+        task['username'] = todo.get('username')
+        todo_list.append(task)
+    json_todos = {}
+    json_todos[emp_id] = todo_list
+    with open(jsonfile, 'w') as output:
+        json.dump(json_todos, output)
+
+
+if __name__ == "__main__":
+    output_json(int(argv[1]))
