4893b60a8db6829d85fe3b38702d110ec970aa56
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-09-29 20:19:47 +0000

    task0 revised

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,11 +2,11 @@
 
 exec { 'increase_fileno_limit':
   command => 'sed -i "s/15/10000/g" /etc/default/nginx',
-  path    => [ '/bin/' ]
+  path    => '/bin/',
   onlyif  => 'test -e /etc/default/nginx',
 }
 
 exec { 'restart nginx':
   command => 'service nginx restart',
-  path    => [ '/usr/bin/' ]
+  path    => '/usr/bin/',
 }
